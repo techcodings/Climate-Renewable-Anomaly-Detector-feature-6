ef998150299329d3752724217c9da47dcb334962
techcodings <161327197+techcodings@users.noreply.github.com>
2025-11-05 10:30:48 +0530

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,7 +5,7 @@ import SeverityTable from "../components/SeverityTable";
 import MapImpact from "../components/MapImpact";
 import Alerts from "../components/Alerts";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:8002";
+const API = import.meta.env.VITE_API_URL || "https://mellow-sundae-ab0bf2.netlify.app/.netlify/functions";
 
 export default function App() {
   const [data, setData] = useState(null);
